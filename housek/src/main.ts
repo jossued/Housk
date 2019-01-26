2e52818e673d3c5a177934533bc4334ca2e2b78c
Jossué Dután <jossuedutan@gmail.com>
2019-01-26 13:17:48 -0500

    instalado express y ejs

--- housek/src/main.ts
+++ housek/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import * as express from 'express';
+import * as session from 'express-session';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(express.static('publico'));
+    app.set('view engine', 'ejs');
+    await app.listen(3000);
 }
+
 bootstrap();
